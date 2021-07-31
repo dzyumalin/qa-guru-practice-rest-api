efd723e3ff6f2532c704929fded3eabc505a73f0
Zyumalin Dmitriy <zyumalin@bk.ru>
2021-07-31 16:32:39 +0300

    intial commit

---
+++ src/main/java/com/example/qagururestbackend/QaGuruRestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.qagururestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaGuruRestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaGuruRestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qagururestbackend/QaGuruRestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qagururestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaGuruRestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
